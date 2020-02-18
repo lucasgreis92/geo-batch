ffdc80b111ce90f39fd754875429c7462e4323f2
Lucas <lucas.reis@redequalis.com.br>
2020-02-17 22:01:06 -0300

    1.0.0

--- src/main/java/br/com/spintec/logicae/geobatch/service/SensorsTmpService.java
+++ src/main/java/br/com/spintec/logicae/geobatch/service/SensorsTmpService.java
@@ -4,6 +4,10 @@ import br.com.spintec.logicae.geobatch.dto.SensorsDtoV1;
 import br.com.spintec.logicae.geobatch.mapper.SensorsTmpMapper;
 import br.com.spintec.logicae.geobatch.model.SensorsTmp;
 import br.com.spintec.logicae.geobatch.repository.SensorsTmpRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -24,6 +28,8 @@ public class SensorsTmpService {
         return sensorsTmpRepository.findAll();
     }
 
+    final static Logger log = LoggerFactory.getLogger(LesenseBatchService.class);
+
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void deleteAll(List<SensorsTmp> sensores) {
         sensorsTmpRepository.deleteAll(sensores);
@@ -31,6 +37,17 @@ public class SensorsTmpService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public List<SensorsTmp> savelAll(List<SensorsDtoV1> sensores){
-        return sensorsTmpRepository.saveAll(sensorsTmpMapper.convertToModelList(sensores));
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<SensorsTmp> tmps = sensorsTmpMapper.convertToModelList(sensores);
+        try {
+            log.info("################################# recived ##################################");
+            log.info(objectMapper.writeValueAsString(sensores));
+            log.info("################################# tmps ##################################");
+            log.info(objectMapper.writeValueAsString(tmps));
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return sensorsTmpRepository.saveAll(tmps);
     }
 }
