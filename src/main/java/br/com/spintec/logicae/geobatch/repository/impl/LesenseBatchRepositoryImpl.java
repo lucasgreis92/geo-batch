7acd96b978985ba36db2e23c1a5b8236f69811f9
Lucas <lucas.reis@redequalis.com.br>
2020-02-27 03:18:57 -0300

    1.0.0

--- src/main/java/br/com/spintec/logicae/geobatch/repository/impl/LesenseBatchRepositoryImpl.java
+++ src/main/java/br/com/spintec/logicae/geobatch/repository/impl/LesenseBatchRepositoryImpl.java
@@ -4,6 +4,8 @@ import br.com.spintec.logicae.geobatch.repository.custom.LesenseBatchRepositoryC
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
 import javax.persistence.StoredProcedureQuery;
 
@@ -17,6 +19,7 @@ public class LesenseBatchRepositoryImpl implements LesenseBatchRepositoryCustom 
     @Override
     public void generateData(String serial) {
         StoredProcedureQuery proc = em.createStoredProcedureQuery("generate_data");
+        proc.registerStoredProcedureParameter("serial_p",String.class, ParameterMode.IN);
         proc.setParameter("serial_p",serial);
         proc.executeUpdate();
     }
