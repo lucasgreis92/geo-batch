217df974c59fe57c6021a153a27b020aa62819aa
Lucas Reis <lucasgreis92@gmail.com>
2020-03-23 20:15:30 -0300

    1.1.0

---
+++ src/main/java/br/com/spintec/logicae/geobatch/controller/v1/GenerateDataControllerV1.java
@@ -0,0 +1,18 @@
+package br.com.spintec.logicae.geobatch.controller.v1;
+
+import br.com.spintec.logicae.geobatch.service.LesenseBatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateDataControllerV1 {
+
+    @Autowired
+    private LesenseBatchService lesenseBatchService;
+
+    @Scheduled(cron = "*/30 * * ? * *")
+    public void generateData() {
+        lesenseBatchService.generateData();
+    }
+}
