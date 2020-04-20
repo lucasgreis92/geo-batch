63ad1d1daa07e19338d15810b3e0663db69050c6
Lucas Reis <lucasgreis92@gmail.com>
2020-04-20 16:35:38 -0300

    1.0.0

--- src/main/java/br/com/spintec/logicae/geobatch/service/LesenseBatchService.java
+++ src/main/java/br/com/spintec/logicae/geobatch/service/LesenseBatchService.java
@@ -179,14 +179,14 @@ public class LesenseBatchService {
             Optional<CalibrationData> lastCd = cdList.stream()
                     .findFirst();
             if (lastCd.isPresent()) {
-                if (!lastCd.get().isHumidityM2()) {
+                if (lastCd.get().isHumidityM2()) {
                     step = 2;
-                } else if (!lastCd.get().isHumidityP2()) {
+                } else if (lastCd.get().isHumidityP2()) {
                     step = 3;
-                } else if (!lastCd.get().isHumidityP4()) {
+                } else if (lastCd.get().isHumidityP4()) {
                     step = 4;
                 } else {
-                    step = 0;
+                    step = 1;
                 }
             }
             if (step > 0) {
